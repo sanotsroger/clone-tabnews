5ca31b804d4693f9a5533bad61bbff155c0fa325
Roger Santos <rogerioantoniodossantos@outlook.com>
2024-05-21 21:07:16 -0300

    Sync files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should be return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const databaseVersionValue = responseBody.dependencies.database.version;
+  const databaseMaxConnetionsValue =
+    responseBody.dependencies.database.max_connections;
+  const databaseOpenedConnetionsValue =
+    responseBody.dependencies.database.opened_connections;
+
+  expect(databaseVersionValue).toBe("16.0");
+  expect(databaseMaxConnetionsValue).toBe(100);
+  expect(databaseOpenedConnetionsValue).toBe(1);
 });
