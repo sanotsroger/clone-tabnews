744628fe80d0dec03e42a53766afa8b7bddfaf17
Roger Santos <rogerioantoniodossantos@outlook.com>
2024-09-22 12:35:21 -0300

    adds endpoint migrations

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,37 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultConfig = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultConfig);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultConfig,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
